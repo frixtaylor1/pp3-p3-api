b77c05906404d047c48fe0f2c23ea8cf9092022f
Omarlopezw <omarlopezxs@gmail.com>
2023-09-25 21:05:51 -0300

    created UserHandler

--- Handlers/userHandler.js
+++ Handlers/userHandler.js
@@ -1,52 +1,65 @@
-/* 
-	CRUD de la entidad 'User'
-	La clase userHandler contiene una serie de atributos que serán datos personales que se le solicitará
-	al usuario. También contiene una serie de métodos que serán cada unas de de las fases del CRUD:
-	userCreate, userRead, userUpdate y userDelete.
-*/
-
-class userHandler
+class UserHandler
 {
-	constructor(userID, username, password)
-	{
-		this.userID = userID;
-		this.username = username;
-		this.password = password;
-	}
+    constructor(DBHandler)
+    {
+        this.DBHandler = DBHandler;
+    }
 
-	userCreate(ID, user, pass)
-	{
-        // console.log('Crear usuario: ' + this.userID + ' ' + this.username + ' ' + this.password);
-        this.userID = ID;
-        this.username = user;
-        this.password = pass;
-	}
+    /**
+     *  Crea un usuario...
+     * 
+     * @param object userData // username,password
+     * @return object
+     **/
+    async create(userData)
+    {
+        let results = {};
+        results = await this.DBHandler.executeStoreProcedure('usp_create_user',userData);
 
-	userRead()
-	{
-		return this.userID;
-		return this.username;
-		return this.password;
-	}
+        return results;
+    }
 
-	userUpdate()
-	{
+    /**
+     *  elimina un usuario...
+     * 
+     * @param object userData // userid
+     * @return object
+     **/
+    async remove(userData)
+    {
+        let results = {};
+        results = await this.DBHandler.executeStoreProcedure('usp_remove_user',userData);
 
-	}
+        return results;
+    }
 
-	userDelete()
-	{
+    /**
+     *  obtiene información de un usuario...
+     * 
+     * @param object userData // userid
+     * @return object
+     **/
+    async read(userData)
+    {
+        let results = {};
+        results = await this.DBHandler.executeStoreProcedure('usp_read_user',userData);
 
-	}
+        return results;
+    }
+    /**
+     *  actualiza información de un usuario...
+     * 
+     * @param object userData // name,surname,DNI,mail,birthdate,age
+     * @return object
+     **/
+    async update(userData)
+    {
+        let results = {};
+        results = await this.DBHandler.executeStoreProcedure('usp_update_user',userData);
+
+        return results;
+    }
+
 }
 
-//Creo el primer usuario
-let userOne = new userHandler('01', 'nacho223', '1234');
-userOne.userID = 1;
-userOne.username = 'nacho223';
-userOne.password = '1234';
-
-//Leo el usuario
-console.log(userOne.ID);
-console.log(userOne.user);
-console.log(userOne.pass);
+module.exports = { UserHandler }
